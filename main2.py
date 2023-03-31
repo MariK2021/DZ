74d03f1a20d8c408dd253c3045845ee54dd69ba2
MaRcoB85 <71268709+MaRcoB85@users.noreply.github.com>
2023-03-31 15:37:57 +0300

    changes dz2 after help

--- main2.py
+++ main2.py
@@ -5,6 +5,9 @@ adress= {
     countries[2]: 'Rome'
 }
 
-for key, value in adress.items():
-    print(key + ": " + value)
+#for key, value in adress.items():
+   # print(key + ": " + value)
 
+print(countries[0] + ": " + adress[countries[0]])
+print(countries[1] + ": " + adress[countries[1]])
+print(countries[2] + ": " + adress[countries[2]])
