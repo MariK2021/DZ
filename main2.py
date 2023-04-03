aab5224c180bda9ddcfbd206c3475bdf7023c164
MaRcoB85 <71268709+MaRcoB85@users.noreply.github.com>
2023-04-03 23:44:36 +0300

    to main branch

--- main2.py
+++ main2.py
@@ -4,10 +4,10 @@ adress= {
     countries[1]: 'Madrid',
     countries[2]: 'Rome'
 }
-
 print(countries[0] + ": " + adress[countries[0]])
 print(countries[1] + ": " + adress[countries[1]])
 print(countries[2] + ": " + adress[countries[2]])
 
 #for key, value in adress.items():
    # print(key + ": " + value)
+
