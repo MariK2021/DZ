c82a6da057436ec8048ce6ac68f83b08bd49635f
MaRcoB85 <71268709+MaRcoB85@users.noreply.github.com>
2023-04-04 13:07:12 +0300

    move to main

---
+++ main6.py
@@ -0,0 +1,11 @@
+a = int(input("Enter height of rectangular: "))
+b = int(input("Enter width of rectangular: "))
+c = str(input("Enter symbol to build rectangular with: "))
+d = c*b
+while a != 0:
+    print(d)
+    a -= 1
+
+
+
+
