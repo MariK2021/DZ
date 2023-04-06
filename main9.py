30b70fe47684f5a0980e34ed6281096b8d890ded
MaRcoB85 <71268709+MaRcoB85@users.noreply.github.com>
2023-04-06 12:51:36 +0300

    changes

--- main9.py
+++ main9.py
@@ -1,12 +1,15 @@
 a = int(input("Enter n: "))
 if a < 0:
     print("Enter a positive number")
+if a == 0:
+    print(a)
 for i in range(1, a+1):
     for j in range(a-i):
         print(" " * 2, end="")
-    for j in range(1, i+1):
+    for j in range(1, i):
         print(j, end=" ")
-    for j in range(i-1, 0, -1):
-        print(j, end=" ")
-    print()
-
+    for j in range(i, 0, -1):
+        if j == 1:
+            print("1")
+        else:
+            print(j, end=" ")
