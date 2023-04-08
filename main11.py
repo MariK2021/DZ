ee71c19c0ba71b0255772061b9780b244ae050dd
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-04-08 20:43:37 +0300

    dz11 correct

--- main11.py
+++ main11.py
@@ -1,8 +1,10 @@
 multi_string = "Hello all. Here's pretty cold and hot. Choose yourself."
-words = multi_string.split(". ")
+words = multi_string.split(" .") and multi_string.split(".")
 words_number = []
 for i in words:
     a = i.split()
     quantity = len(a)
+    if len(a) < 1:
+        continue
     words_number.append(quantity)
 print("words_number ->", words_number)
