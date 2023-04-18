5d8213a578847c605d9adfa01c2f9a47f00e2372
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-04-18 13:16:31 +0300

    dz17 correct

--- main17.py
+++ main17.py
@@ -1,14 +1,14 @@
 lst = [['a', 'c', 'd'], ['f', 'b', 'a'], ['a', 'n', 'k'], ['e', 'l', 'i']]
-lst2 = [i[0] for i in lst]
-lst2.sort()
-lst3 = [i[1] for i in lst]
-lst3.sort()
-lst4 = [i[2] for i in lst]
-lst4.sort()
-new_lst = [lst2, lst3, lst4]
-new_lst2 = [i[0] for i in new_lst]
-new_lst3 = [i[1] for i in new_lst]
-new_lst4 = [i[2] for i in new_lst]
-new_lst5 = [i[3] for i in new_lst]
-final_lst = [new_lst2, new_lst3, new_lst4, new_lst5]
+columns = len(lst[0])
+new_lst = [[] for i in range(columns)]
+for i in range(columns):
+    for j in range(len(lst)):
+        new_lst[i].append(lst[j][i])
+        new_lst[i].sort()
+
+columns2 = len(new_lst[0])
+final_lst = [[] for i in range(columns2)]
+for i in range(columns2):
+    for j in range(len(new_lst)):
+        final_lst[i].append(new_lst[j][i])
 print(final_lst)
