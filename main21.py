8602de875a022675fac5922e748cbde754afe7a3
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-04-21 22:27:03 +0300

    dz21

---
+++ main21.py
@@ -0,0 +1,38 @@
+# file = open('dz21.txt', 'w')
+
+# read file
+with open('dz21.txt', 'r') as file:
+    lines = file.readlines()
+
+# create dict
+users = []
+for line in lines:
+    line.split(";")
+    users.append(line)
+
+import json
+result = []
+for i in users:
+    fields = i.strip().split(';')
+    name = fields[0].strip()
+    if len(fields) >= 2 and fields[1].strip().isdigit():
+        age = int(fields[1].strip())
+    else:
+        age = None
+    phones = ['+' + phone.strip(' +') for phone in fields[2].split(',') if phone.strip()] if len(fields) == 3 else []
+    result.append({'name': name, 'age': age, 'phones': phones})
+print(result)
+
+# json
+with open('result.json', "w") as f:
+    json.dump(result, f)
+
+# out-file
+with open('out_file.txt', "w") as f:
+    for user in result:
+        name = user['name'] if user['name'] else ''
+        age = str(user['age']) if user['age'] else ''
+        phones = ','.join([str(phone) for phone in user['phones']]) if user['phones'] else ''
+        if name or age or phones:
+            f.write(';'.join([name, age, phones]))
+            f.write('\n')
