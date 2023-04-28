f09e44453d1222e9e772b1888c9b782ecef2708d
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-04-28 17:03:45 +0300

    dz28

--- main28.py
+++ main28.py
@@ -5,9 +5,9 @@ def generate_sentence(text):
     pattern = r"[A-Z][^\.!?]*"
     result = re.findall(pattern, text)
     first_words = []
-    for s in result:
-        s = s.split()[0]
-        first_words.append(s.lower())
+    for i in result:
+        i = i.split()[0]
+        first_words.append(i)
     last_version = ' '.join(first_words).capitalize()
     return f'"{last_version}."'
 
