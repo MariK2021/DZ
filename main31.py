24a5a59968fddb324b4415515e65ddcbbff4e244
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-05-03 17:12:09 +0300

    dz31

---
+++ main31.py
@@ -0,0 +1,12 @@
+import random
+
+def generate_random_string(length):
+    result = ''
+    for i in range(length):
+        random_num = random.randint(48, 122)
+        while not (48 <= random_num <= 57 or 65 <= random_num <= 90 or 97 <= random_num <= 122):
+            random_num = random.randint(48, 122)
+        result += chr(random_num)
+    return result
+
+print(generate_random_string(12))
