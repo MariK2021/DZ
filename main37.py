900262c54a8c5de6a4f1b0c1c11413df8e3acb33
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-05-23 13:24:52 +0300

    dz37

---
+++ main37.py
@@ -0,0 +1,35 @@
+class AttributePrinterMixin:
+    def print_attributes(self):
+        attributes = vars(self)
+        class_name = type(self).__name__
+        attributes_str = ""
+        for key, value in attributes.items():
+            if key.startswith('_'):
+                key = key.split('_')[-1]
+            attributes_str += f"\t{key}: {value}\n"
+        print(f"{class_name}: {{\n{attributes_str}}}")
+
+
+class Order(AttributePrinterMixin):
+    def __init__(self, order_id, article, price):
+        self.order_id = order_id
+        self.article = article
+        self.__price = price
+        self._client = "Hillel"
+
+    @property
+    def total_sum(self):
+        return self.__price
+
+
+class InnerOrder(Order):
+    def __init__(self, order_id, article, price, netto_price):
+        super().__init__(order_id, article, price)
+        self.netto_price = netto_price
+
+
+order = Order(1, 'HUT1GT', 3000)
+innerorder = InnerOrder(1, 'HUT1GT', 3000, 2000)
+
+order.print_attributes()
+innerorder.print_attributes()
