43214cb4691566000c2ab7e91510f7765957608f
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-05-25 14:49:13 +0300

    dx37 correct

--- main37.py
+++ main37.py
@@ -1,30 +1,29 @@
 class AttributePrinterMixin:
+    def __str__(self):
+        return self.print_attributes()
+
     def print_attributes(self):
         attributes = vars(self)
         class_name = type(self).__name__
         attributes_str = ""
         for key, value in attributes.items():
             if key.startswith('_'):
-                key = key.split('_')[-1]
+                key_parts = key.split('__')
+                key = key_parts[-1].lstrip('_')
+                #   key = key[1:]
             attributes_str += f"\t{key}: {value}\n"
-        print(f"{class_name}: {{\n{attributes_str}}}")
+        return f"{class_name}: {{\n{attributes_str}}}"
 
 
-class Order(AttributePrinterMixin):
-    def __init__(self, order_id, article, price):
-        self.order_id = order_id
-        self.article = article
-        self.__price = price
-        self._client = "Hillel"
+class A(AttributePrinterMixin):
+    def __init__(self):
+        self.public_filed = 3
+        self._protected_field = 'q'
+        self.__private_field = [1, 2, 3]
 
-class InnerOrder(Order):
-    def __init__(self, order_id, article, price, netto_price):
-        super().__init__(order_id, article, price)
-        self.netto_price = netto_price
+    def __str__(self):
+        return super().__str__()
 
 
-order = Order(1, 'HUT1GT', 3000)
-innerorder = InnerOrder(1, 'HUT1GT', 3000, 2000)
-
-order.print_attributes()
-innerorder.print_attributes()
+a = A()
+print(a)
