66f54b2850421abc5abe75152ec110eccdbb4c2a
Maryna <71268709+MaRcoB85@users.noreply.github.com>
2023-05-26 10:55:37 +0300

    dz38 correct

--- main38.py
+++ main38.py
@@ -2,21 +2,23 @@ def cache_data(func):
     cache = {}
 
     def wrapper(*args, **kwargs):
-        if args in cache:
-            return cache[args]
+        if (args, tuple(kwargs.items())) in cache:
+            return cache[args, tuple(kwargs.items())]
         else:
             result = func(*args, **kwargs)
-            cache[args] = result
+            cache[args, tuple(kwargs.items())] = result
             return result
+
     return wrapper
 
 
 @cache_data
-def add(a, b):
-    return a+b
+def func_with_kwargs(title, **kwargs):
+    print(f"Calculated for {title} {kwargs}")
+    return sum([kwargs[key] for key in kwargs])
 
 
-print(add(3, 7))
-print(add(3, 7))
-print(add(1, 7))
-print(add(1, 7))
+print(func_with_kwargs('first', a=3, b=4))
+print(func_with_kwargs('first', a=3, b=4))
+print(func_with_kwargs('first', a=100, b=4))
+print(func_with_kwargs('second', a=100, b=4))
